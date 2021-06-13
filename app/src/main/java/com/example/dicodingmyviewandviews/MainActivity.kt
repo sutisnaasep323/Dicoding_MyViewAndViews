603b615f0eb3ecb22e55fe06c1ddb276eb924368
yummicodes <yummicodes@gmail.com>
2021-06-13 15:02:23 +0700

    Edit value support action bar

--- app/src/main/java/com/example/dicodingmyviewandviews/MainActivity.kt
+++ app/src/main/java/com/example/dicodingmyviewandviews/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportActionBar?.title = "Dicoding_MyViewAndViews"
+        supportActionBar?.title = "Google Pixel"
     }
 }
